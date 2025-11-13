50383add4bc8ed6a4772afc2623838431afdb825
apb-nscc <86730691+stellartortoise@users.noreply.github.com>
2025-11-13 14:16:42 -0400

    Migrated Purchases

---
+++ NSCC-WebAppProg-SeatYourself/Migrations/20251113181046_AddedPurchaseModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NSCC_WebAppProg_SeatYourself.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPurchaseModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Purchase",
+                columns: table => new
+                {
+                    PurchaseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumTicketsOrdered = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerFirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerLastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerPhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreditCardNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreditCardExpiry = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreditCardCvv = table.Column<int>(type: "int", nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OccasionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Purchase", x => x.PurchaseId);
+                    table.ForeignKey(
+                        name: "FK_Purchase_Occasion_OccasionId",
+                        column: x => x.OccasionId,
+                        principalTable: "Occasion",
+                        principalColumn: "OccasionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchase_OccasionId",
+                table: "Purchase",
+                column: "OccasionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Purchase");
+        }
+    }
+}
